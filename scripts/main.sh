8b04b58233b1c94ad31615b43f278bf971ba114c
zfsdk <zfsdk@163.com>
2024-01-23 23:27:29 +0800

    继续

--- scripts/main.sh
+++ scripts/main.sh
@@ -28,7 +28,7 @@ copy_pantherx2_uboot_files() {
 	# display_alert "Copying pantherx2 u-boot files to:" "$distdir" "info"
 
 	cp "${EXTER}"/packages/blobs/pantherx2/rk3566-pantherx2_defconfig  "${distdir}"/configs/rk3566-pantherx2_defconfig
-	cp "${EXTER}"/packages/blobs/pantherx2/rk3566-pantherx2.dts  "${distdir}"/arch/arm/dts/rk3566-pantherx2.dts
+	cp "${EXTER}"/packages/blobs/pantherx2/rk3566-pantherx2-uboot.dts  "${distdir}"/arch/arm/dts/rk3566-pantherx2.dts
 }
 
 copy_pantherx2_kernel_files() {
